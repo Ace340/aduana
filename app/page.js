65441ef9867a8226a0117c52b388dae22c085121
Ace340 <acevedojf2@gmail.com>
2024-08-24 18:25:33 -0400

    implement footer feature

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
 import Whatsapp from "@/components/Whatsapp";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <About />
       <Services />
       <Contact />
+      <Footer />
     </main>
   );
 }
